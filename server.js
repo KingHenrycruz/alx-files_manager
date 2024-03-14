8630af51b7dcd9e1848ddf9f6e101f040d1e9aca
KingHenrycruz <KingHenrycruz@gmail.com>
2024-03-14 15:07:46 +0000

    test Update

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 import express from 'express';
 import controllerRouting from './routes/index';
+import injectRoutes from './routes';
+import injectMiddlewares from './libs/middlewares';
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
 controllerRouting(app);
+injectMiddlewares(app);
+injectRoutes(app);
+startServer(app);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
